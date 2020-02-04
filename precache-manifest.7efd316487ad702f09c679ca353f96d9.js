3147d1f441874be024a406e932e2b22e4a437baa
mate <bob@policmate@gmail.com>
2020-02-04 14:34:47 +0100

    Updates

---
+++ precache-manifest.7efd316487ad702f09c679ca353f96d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f317f3cae89272512c0af29e3226869",
+    "url": "/Projekt-recepti/index.html"
+  },
+  {
+    "revision": "12a711e91bdf166e5548",
+    "url": "/Projekt-recepti/static/css/main.3c0bd68a.chunk.css"
+  },
+  {
+    "revision": "003277ea0a95f1b3ce2c",
+    "url": "/Projekt-recepti/static/js/2.24ee7dfa.chunk.js"
+  },
+  {
+    "revision": "12a711e91bdf166e5548",
+    "url": "/Projekt-recepti/static/js/main.1e55b8f7.chunk.js"
+  },
+  {
+    "revision": "1ac95ed5cd7b8f221443",
+    "url": "/Projekt-recepti/static/js/runtime~main.c9a7f358.js"
+  }
+]);
